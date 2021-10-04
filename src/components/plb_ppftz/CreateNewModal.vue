f16d2e55086ce16387e20f1db7f9b75bd427473b
Ahmad <ahmads1704@gmail.com>
2021-10-04 22:24:34 +0700

    create form

--- src/components/plb_ppftz/CreateNewModal.vue
+++ src/components/plb_ppftz/CreateNewModal.vue
@@ -1,34 +1,108 @@
 <template>
   <v-card>
     <v-card-title>
-      <span class="text-h6">Created New</span>
+      <v-row no-gutters align-content="center" justify="space-between">
+        <span class="text-h6">Created New</span>
+        <span style="cursor: pointer" @click.prevent="handleDialog"
+          ><v-icon>mdi-close</v-icon></span
+        >
+      </v-row>
     </v-card-title>
     <v-card-text>
-      <v-container>
-        ss
-      </v-container>
+      <v-form
+        ref="form"
+        @submit.prevent="handleSubmit"
+        v-model="formValid"
+        lazy-validation
+      >
+        <v-container>
+          <v-select
+            dense
+            v-model="pengajuanSebagai"
+            label="Pengajuan Sebagai"
+            :items="['Test 1', 'Test 2']"
+            outlined
+            :rules="requiredRules"
+          ></v-select>
+          <v-select
+            dense
+            label="Diajukan Dikantor"
+            :items="['Test 1', 'Test 2']"
+            outlined
+          ></v-select>
+          <v-select
+            dense
+            label="Jenis Pemberitahuan"
+            :items="['Test 1', 'Test 2']"
+            outlined
+          ></v-select>
+          <v-select
+            dense
+            label="Jenis Dokumen BC"
+            :items="['Test 1', 'Test 2']"
+            outlined
+          ></v-select>
+        </v-container>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <button
+            type="submit"
+            class="btn_save"
+            id="submit-form"
+            :disabled="!formValid"
+          >
+            <span>Next</span> <img src="../../assets/icons/ic_bulletnext.svg" />
+          </button>
+        </v-card-actions>
+      </v-form>
     </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="blue darken-1" text @click.prevent="handleDialog">
-        Close
-      </v-btn>
-      <v-btn color="blue darken-1" text @click.prevent="handleDialog">
-        Save
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
 <script>
 export default {
   name: "CreateNewModal",
+  data() {
+    return {
+      formValid: false,
+      pengajuanSebagai: "",
+      diajukanDikantor: "",
+      jenisPemberitahuan: "",
+      jenisDokumenBC: "",
+      requiredRules: [(v) => !!v || `Pengajuan sebagai harus diisi`],
+    };
+  },
+  watch: {
+    pengajuanSebagai(val) {
+      console.log(val);
+    },
+  },
   methods: {
     handleDialog() {
       this.$emit("handleModal");
+    },
+    handleSubmit() {
+      this.$refs.form.validate();
+      console.log("test");
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.btn_save {
+  background-color: transparent;
+  width: max-content;
+  outline: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  user-select: none;
+  span {
+    font-size: 1rem;
+    color: #9ba1bc;
+  }
+}
+</style>
