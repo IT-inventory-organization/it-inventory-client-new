e1a60dfebb187c1265d19f6885768251d45c12f0
programmer-kuadran <82074322+programmer-kuadran@users.noreply.github.com>
2021-11-09 16:51:19 +0700

    inisialisasi dashboard baru

---
+++ src/components/base/NavMenu.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="nav_app" id="nav-app">
+    <div
+      v-for="(item, index) in menus"
+      :key="index"
+      class="nav_app_menu_wrapper"
+    >
+      <router-link
+        class="nav_app_menu_item"
+        v-bind:to="item.path"
+        style="text-decoration:none"
+      >
+        <Icon v-bind:icon="item.icon" class="nav_app_menu_item_icon" />
+        {{ item.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+import { Icon } from "@iconify/vue2";
+export default {
+  name: "NavMenu",
+  components: {
+    Icon,
+  },
+  data: () => ({
+    menus: [
+      {
+        name: "DASHBOARD",
+        path: "/dashboard",
+        icon: "icon-park-outline:dashboard",
+      },
+      {
+        name: "MASTER DATA",
+        path: "/master-data",
+        icon: "feather:database",
+      },
+      {
+        name: "PLB",
+        path: "/plb",
+        icon: "fluent:document-text-20-regular",
+      },
+      {
+        name: "PO",
+        path: "/po",
+        icon: "fluent:document-text-20-regular",
+      },
+      {
+        name: "INVENTORY",
+        path: "/inventory",
+        icon: "bx:bx-box",
+      },
+      {
+        name: "LAPORAN",
+        path: "/laporan",
+        icon: "mdi:file-chart-outline",
+      },
+    ],
+  }),
+  mounted() {
+    // this.stickyNav();
+  },
+  methods: {
+    stickyNav() {
+      window.onscroll = function() {
+        stickyNav();
+      };
+
+      const header = document.getElementById("nav-app");
+      const sticky = header.offsetTop;
+
+      function stickyNav() {
+        if (window.pageYOffset > sticky) {
+          header.classList.add("sticky");
+        } else {
+          header.classList.remove("sticky");
+        }
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "../../assets/styles/header.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,42 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Dashboard from "@/views/Dashboard";
+import MasterData from "@/views/MasterData";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    
+    path: "/",
+    redirect: { name: "Dashboard" },
   },
   {
-    path: '/login',
-    name: 'login',
-    
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: '/register',
-    name: 'register',
+    path: "/master-data",
+    name: "MasterData",
+    component: MasterData,
   },
-]
+  {
+    path: "/login",
+    name: "login",
+  },
+  {
+    path: "/register",
+    name: "register",
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
