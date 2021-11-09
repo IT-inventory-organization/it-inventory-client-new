a4a8459d77efe0c2683d5421cbc50de09d129c49
programmer-kuadran <82074322+programmer-kuadran@users.noreply.github.com>
2021-11-09 16:51:44 +0700

    membuat halaman dashboard

---
+++ src/components/dashboard/DisplayCount.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="it-inventory-box mt-4">
+    <v-row align="center">
+      <v-col lg="10">
+        <div class="display-1 font-weight-bold">Jumlah Kapal</div>
+      </v-col>
+      <v-col lg="2 text-right">
+        <div class="display-2 font-weight-bold" style="color: #2ED477">
+          100
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DislayCount",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/dashboard/ListKapal.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="mt-12">
+    <div class="headline">List Kapal</div>
+    <v-row no-gutters class="mt-2">
+      <v-col lg="3">
+        <v-text-field placeholder="Cari" class="it-inventory-search-list-kapal">
+          <template slot="prepend-inner">
+            <Icon
+              icon="akar-icons:search"
+              class="mr-2"
+              style="font-size: 1.5rem"
+            />
+          </template>
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="mt-2">
+      <v-col>
+        <div class="it-inventory-box">
+          <v-data-table
+            :headers="headers"
+            :items="data"
+            :items-per-page="5"
+            no-data-text="Data not available"
+            no-results-text="Data not available"
+            class="it-inventory-simple-table"
+          ></v-data-table>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue2";
+export default {
+  name: "ListKapal",
+  components: {
+    Icon,
+  },
+  data() {
+    return {
+      headers: [
+        { text: "Voyage Kapal", value: "voyageKapal" },
+        { text: "Nama Kapal", value: "namaKapal" },
+        { text: "Jenis Dokumen", value: "jenisDokumen" },
+        { text: "Bendera", value: "bendera" },
+      ],
+      data: [
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+        {
+          voyageKapal: "VOYAGE",
+          namaKapal: "Collins - Swift",
+          jenisDokumen: "Pemasukan",
+          bendera: "INA",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
