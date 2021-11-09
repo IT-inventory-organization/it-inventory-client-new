63c97a0d0101282ace684667969aba29aa8308b8
programmer-kuadran <82074322+programmer-kuadran@users.noreply.github.com>
2021-11-09 16:52:00 +0700

    membuat halaman master data

---
+++ src/components/MasterData/DataPemilik.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-col lg="6" sm="12" md="12">
+    <div class="headline font-weight-bold d-flex align-center">
+      <Icon
+        icon="ant-design:user-outlined"
+        style="color: #A6A6A6"
+        class="mr-5"
+      />
+      Data Pemilik
+    </div>
+
+    <v-row class="my-12" no-gutters>
+      <v-col cols="3" class="mr-2">
+        <span class="it-inven-dataperusahaan__label"
+          >Nama Pemilik / Penanggung Jawab</span
+        >
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          value="Fletcher Lindgren"
+          class="it-inven-dataperusahaan__value"
+          hide-details
+          dense
+          disabled
+        />
+      </v-col>
+    </v-row>
+    <v-row class="my-12" no-gutters>
+      <v-col cols="3" class="mr-2">
+        <span class="it-inven-dataperusahaan__label">Alamat Pemilik</span>
+      </v-col>
+      <v-col cols="6">
+        <v-textarea
+          value="Menara Kadin Indonesia, Lt.28. Jl.H.R.Rasuna Said Blok X-5 Kav. 02/03 Jakarta 12950 PO BOX 5032 JKTM Jakarta 12700"
+          hide-details
+          class="it-inven-dataperusahaan__value"
+          dense
+          disabled
+          auto-grow
+          rows="1"
+        >
+        </v-textarea>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue2";
+export default {
+  name: "DataPemilik",
+  components: {
+    Icon,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/MasterData.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-row no-gutters align="center">
+      <v-col cols="11" md="">
+        <div class="display-1 font-weight-bold">
+          Informasi Perusahaan
+        </div>
+      </v-col>
+      <v-col cols="1">
+        <button
+          class="it-inventory-btn it-inventory-btn__fw it-inventory-btn__grey"
+          @click="handleEdit"
+        >
+          Edit
+        </button>
+      </v-col>
+    </v-row>
+    <v-row class="mt-5">
+      <data-perusahaan />
+      <data-pemilik />
+    </v-row>
+    <v-dialog persistent v-model="formEdit" width="50%" max-width="1200px">
+      <form-edit-info-perusahaan @handleCloseFormEdit="handleCloseFormEdit" />
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MasterData",
+  components: {
+    DataPerusahaan: () => import("@/components/MasterData/DataPerusahaan"),
+    DataPemilik: () => import("@/components/MasterData/DataPemilik"),
+    FormEditInfoPerusahaan: () =>
+      import("@/components/MasterData/FormEditInfoPerusahaan"),
+  },
+  data() {
+    return {
+      formEdit: false,
+    };
+  },
+  methods: {
+    handleEdit() {
+      this.formEdit = true;
+    },
+    handleCloseFormEdit() {
+      this.formEdit = false;
+    },
+  },
+};
+</script>
+
+<style
+  lang="scss
+    FormEditInfoPerusahaan"
+  scoped
+></style>
