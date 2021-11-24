a2a98b4eb9fe56df4ebe9a7bc6645615463f17ee
programmer-kuadran <82074322+programmer-kuadran@users.noreply.github.com>
2021-11-24 14:23:59 +0700

    membuat tampilan dokumen po

--- src/views/PLB/DokumenPO/index.vue
+++ src/views/PLB/DokumenPO/index.vue
@@ -8,7 +8,8 @@
       </v-col>
     </v-row>
     <v-row justify="center" class="mt-5">
-      <v-col cols="4">
+      <v-col cols="4" style="position:relative">
+        <label class="it-inventory-select-po__label">-Pilih Dokumen PO-</label>
         <v-select
           outlined
           :rules="[
@@ -16,37 +17,56 @@
               return genericRequiredRule(value, 'Pilih Dokumen PO');
             },
           ]"
+          single-line
           :items="itemDokumenPO"
           item-text="name"
           multiple
+          hide-details
+          class="it-inventory-select-po"
           v-model="selectedDokumenPO"
-          placeholder="- Pilih Dokumen PO -"
           append-icon="mdi-chevron-down"
         >
         </v-select>
       </v-col>
     </v-row>
-    <v-row justify="center" class="mt-5">
-      <v-col cols="4">
+    <v-row
+      justify="center"
+      class="mt-5"
+      v-for="(item, index) in selectedDokumenPO"
+      :key="index"
+    >
+      <v-col cols="4" style="position:relative">
         <v-text-field
-          v-for="(item, index) in selectedDokumenPO"
-          :key="index"
           outlined
           :value="item"
-          color="#0C66CF"
+          class="it-inven-dokumen-po__item"
           readonly
+          hide-details
           append-icon="mdi-check"
         >
-          <template v-slot:append-outer>
-            <div @click.prevent="handleView">
-              <Icon class="mx-2" icon="fluent:apps-list-detail-20-regular" />
-            </div>
-            <div @click.prevent="handleDelete">
-              <Icon class="mx-2" icon="octicon:trash-24" />
-            </div>
-          </template>
         </v-text-field>
+        <div class="it-inven-dokumen-po__items-action">
+          <div @click.prevent="handleView">
+            <Icon
+              class="mx-2 it-inven-dokumen-po__items-action__item"
+              icon="fluent:apps-list-detail-20-regular"
+            />
+          </div>
+          <div @click.prevent="handleDelete(item)">
+            <Icon
+              class="mx-2 it-inven-dokumen-po__items-action__item"
+              icon="octicon:trash-24"
+            />
+          </div>
+        </div>
       </v-col>
+    </v-row>
+
+    <v-row no-gutters style="justify-content: flex-end" class="my-12">
+      <button type="submit" class="btn_save" @click.prevent="handleSubmit">
+        <span>Selanjutnya</span>
+        <img src="@/assets/icons/ic_bulletnext.svg" />
+      </button>
     </v-row>
   </div>
 </template>
@@ -74,14 +94,65 @@ export default {
     },
   },
   methods: {
-    handleDelete() {
-      alert("Delete");
+    handleDelete(value) {
+      this.$store.commit("DELETE_SELECTED_DOKUMEN_PO", value);
     },
     handleView() {
       alert("View");
+    },
+    handleSubmit() {
+      if (this.selectedDokumenPO.length > 0) {
+        this.$store.commit("SET_STEPPER", 2);
+        return true;
+      } else {
+        return false;
+      }
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.it-inven-dokumen-po__item {
+  ::v-deep {
+    .v-input__slot {
+      fieldset {
+        border-color: #0c66cf !important;
+      }
+      input {
+        color: #0c66cf !important;
+      }
+      i {
+        color: #0c66cf !important;
+      }
+    }
+  }
+}
+
+.it-inven-dokumen-po__items-action {
+  position: absolute;
+  right: -80px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  &__item {
+    font-size: 24px;
+    cursor: pointer;
+  }
+}
+
+.it-inventory-select-po {
+  ::v-deep {
+    .v-select__selection {
+      display: none !important;
+    }
+  }
+
+  &__label {
+    position: absolute;
+    top: 50%;
+    left: 25px;
+    transform: translateY(-50%);
+  }
+}
+</style>
